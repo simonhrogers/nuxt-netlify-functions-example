c21c3cc6ca5d556f65055c02b92a2dec576cb2c4
fm11 <fm11@fm11s-iMac.home>
2019-11-04 12:31:37 +0000

    put gateway.js contents into function itself (test)

--- functions/braintree-get-token.js
+++ functions/braintree-get-token.js
@@ -1,4 +1,24 @@
-import gateway from '/gateway.js';
+// import gateway from '/gateway.js';
+
+import { connect, Environment } from 'braintree';
+require('dotenv').config();
+
+const {
+  BT_ENVIRONMENT,
+  BT_MERCHANT_ID,
+  BT_PUBLIC_KEY,
+  BT_PRIVATE_KEY
+} = process.env;
+
+const environment =
+  BT_ENVIRONMENT.charAt(0).toUpperCase() + BT_ENVIRONMENT.slice(1);
+
+const gateway = connect({
+  environment: Environment[environment],
+  merchantId: BT_MERCHANT_ID,
+  publicKey: BT_PUBLIC_KEY,
+  privateKey: BT_PRIVATE_KEY
+});
 
 export async function handler(event, context) {
 
