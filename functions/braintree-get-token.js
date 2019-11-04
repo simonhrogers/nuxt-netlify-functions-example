d37e715c3e646f22714716e5d9c9e835cd096f38
fm11 <fm11@fm11s-iMac.home>
2019-11-04 11:41:54 +0000

    ehh 2.0

--- functions/braintree-get-token.js
+++ functions/braintree-get-token.js
@@ -3,26 +3,25 @@
 import { connect, Environment } from 'braintree';
 require('dotenv').config();
 
+const {
+  BT_ENVIRONMENT,
+  BT_MERCHANT_ID,
+  BT_PUBLIC_KEY,
+  BT_PRIVATE_KEY
+} = process.env;
+
+const environment =
+  BT_ENVIRONMENT.charAt(0).toUpperCase() + BT_ENVIRONMENT.slice(1);
+
+const gateway = connect({
+  environment: Environment[environment],
+  merchantId: BT_MERCHANT_ID,
+  publicKey: BT_PUBLIC_KEY,
+  privateKey: BT_PRIVATE_KEY
+});
 
 export async function handler(event, context) {
-
-  const {
-    BT_ENVIRONMENT,
-    BT_MERCHANT_ID,
-    BT_PUBLIC_KEY,
-    BT_PRIVATE_KEY
-  } = process.env;
-
-  const environment =
-    BT_ENVIRONMENT.charAt(0).toUpperCase() + BT_ENVIRONMENT.slice(1);
-
-  const gateway = connect({
-    environment: Environment[environment],
-    merchantId: BT_MERCHANT_ID,
-    publicKey: BT_PUBLIC_KEY,
-    privateKey: BT_PRIVATE_KEY
-  });
-
+  
   const response = await gateway.clientToken.generate();
 
   const clientToken = response.clientToken;
