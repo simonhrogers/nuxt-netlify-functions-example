e98336de45a76002b642c71394bb9bf108b876fb
fm11 <fm11@fm11s-iMac.home>
2019-11-04 12:00:00 +0000

    move gateway.js to static

--- functions/braintree-get-token.js
+++ functions/braintree-get-token.js
@@ -1,27 +1,7 @@
-// import gateway from './../lib/gateway.js';
-
-import { connect, Environment } from 'braintree';
-require('dotenv').config();
-
-const {
-  BT_ENVIRONMENT,
-  BT_MERCHANT_ID,
-  BT_PUBLIC_KEY,
-  BT_PRIVATE_KEY
-} = process.env;
-
-const environment =
-  BT_ENVIRONMENT.charAt(0).toUpperCase() + BT_ENVIRONMENT.slice(1);
-
-const gateway = connect({
-  environment: Environment[environment],
-  merchantId: BT_MERCHANT_ID,
-  publicKey: BT_PUBLIC_KEY,
-  privateKey: BT_PRIVATE_KEY
-});
+import gateway from '/gateway.js';
 
 export async function handler(event, context) {
-  
+
   const response = await gateway.clientToken.generate();
 
   const clientToken = response.clientToken;
